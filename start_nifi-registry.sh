396fd14b7dbc634d6cdd103dde9feb783340c332
Viacheslav Kalashnikov <xemuliam@users.noreply.github.com>
2018-03-01 10:32:39 +0200

    0.1.0

--- start_nifi-registry.sh
+++ start_nifi-registry.sh
@@ -1,1 +1,8 @@
-1
+#!/bin/sh
+
+set -e
+
+sed -i "s/nifi\.registry\.web\.http\.host=.*/nifi.registry.web.http.host=${HOSTNAME}/g" ${NIFI_REGISTRY_HOME}/conf/nifi-registry.properties
+
+tail -F ${NIFI_REGISTRY_HOME}/logs/nifi-registry-app.log &
+${NIFI_REGISTRY_HOME}/bin/nifi-registry.sh run
